5f099d1b4d1b56f7f6266a60d9c37d6e02461db3
gayloo <120192094+gayloo@users.noreply.github.com>
2024-05-05 18:47:07 -0400

    Create pvz.lua

---
+++ pvz.lua
@@ -0,0 +1,63 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))();
+loadstring(game:HttpGet("https://pastebin.com/raw/PqBt2KtX"))(); -- no buggy graphics
+
+
+local uis = game:GetService("UserInputService")
+uis.InputBegan:Connect(function(input, H)
+  if input.KeyCode == Enum.KeyCode.H then
+game.Players.LocalPlayer.PlayerGui.TeamChangeGUI.Switch.Visible = true
+settings().Rendering.QualityLevel = 10
+  end
+end)
+
+loadstring(game:HttpGet("https://pastebin.com/raw/3r3D2tRT"))(); -- explorer
+
+uis.InputBegan:Connect(function(input, G)
+  if input.KeyCode == Enum.KeyCode.G then
+for i, sun in pairs(workspace:GetChildren()) do
+if game.Players.LocalPlayer.Character.Torso ~= nil then
+if sun.Name == "PlantFood" then
+firetouchinterest(game.Players.LocalPlayer.Character.Torso, sun.Sun, 0)
+elseif sun.Name == "Sun" then
+firetouchinterest(game.Players.LocalPlayer.Character.Torso, sun.Sun, 0)
+elseif sun.Name == "SunBig" then
+firetouchinterest(game.Players.LocalPlayer.Character.Torso, sun.Sun, 0)
+elseif sun.Name == "RainingSun" then
+firetouchinterest(game.Players.LocalPlayer.Character.Torso, sun.Sun, 0)
+elseif sun.Name == "GoldCoin" then
+firetouchinterest(game.Players.LocalPlayer.Character.Torso, sun.Sun, 0)
+elseif sun.Name == "SilverCoin" then
+firetouchinterest(game.Players.LocalPlayer.Character.Torso, sun.Sun, 0)
+elseif sun.Name == "Gem" then
+firetouchinterest(game.Players.LocalPlayer.Character.Torso, sun.Sun, 0)
+settings().Rendering.QualityLevel = 10
+  end
+  end
+  end
+  end
+end)
+
+while true do
+wait(0.1)
+if game.Players.LocalPlayer.Character.Torso ~= nil then
+for i, sun in pairs(workspace:GetChildren()) do
+if game.Players.LocalPlayer.Character.Torso ~= nil then
+if sun.Name == "PlantFood" then
+firetouchinterest(game.Players.LocalPlayer.Character.Torso, sun.Sun, 0)
+elseif sun.Name == "Sun" then
+firetouchinterest(game.Players.LocalPlayer.Character.Torso, sun.Sun, 0)
+elseif sun.Name == "SunBig" then
+firetouchinterest(game.Players.LocalPlayer.Character.Torso, sun.Sun, 0)
+elseif sun.Name == "RainingSun" then
+firetouchinterest(game.Players.LocalPlayer.Character.Torso, sun.Sun, 0)
+elseif sun.Name == "GoldCoin" then
+firetouchinterest(game.Players.LocalPlayer.Character.Torso, sun.Sun, 0)
+elseif sun.Name == "SilverCoin" then
+firetouchinterest(game.Players.LocalPlayer.Character.Torso, sun.Sun, 0)
+elseif sun.Name == "Gem" then
+firetouchinterest(game.Players.LocalPlayer.Character.Torso, sun.Sun, 0)
+end
+end
+end
+end
+end
