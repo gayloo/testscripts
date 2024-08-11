c18c70216bcb2201eb6561d1be1a38bb7e0caf16
gayloo <120192094+gayloo@users.noreply.github.com>
2024-08-10 21:08:21 -0400

    Update and rename dandyjoe.lua to dandyjoe2.lua

--- dandyjoe2.lua
+++ dandyjoe2.lua
@@ -1,5 +1,5 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "toe v0.1",IntroText = "toe, you! again", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = "toe v0.101",IntroText = "toe, you! again", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
 local tab = Window:MakeTab({
 	Name = "Main",
